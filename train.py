e7b5e365a7b26c9cdf3fb51fe837cd784cbc4d80
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2017-11-15 17:43:20 +0900

    add a script that is an entry-point

---
+++ train.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python
+
+import argparse
+
+from nltk.translate import bleu_score
+import numpy
+import progressbar
+import six
+
+import chainer
+from chainer import cuda
+from chainer.dataset import convert
+import chainer.functions as F
+import chainer.links as L
+from chainer import training
+from chainer.training import extensions
+
+from net import AttentionSeq2seq
+
+
+UNK = 0
+EOS = 1
+BOS = 2
+
+
+def seq2seq_pad_concat_convert(xy_batch, device, eos_id=EOS, bos_id=BOS):
+    """
+    Args:
+        xy_batch (list of tuple of two numpy.ndarray-s or cupy.ndarray-s):
+            xy_batch[i][0] is an array
+            of token ids of i-th input sentence in a minibatch.
+            xy_batch[i][1] is an array
+            of token ids of i-th target sentence in a minibatch.
+            The shape of each array is `(sentence length, )`.
+        device (int or None): Device ID to which an array is sent. If it is
+            negative value, an array is sent to CPU. If it is positive, an
+            array is sent to GPU with the given ID. If it is ``None``, an
+            array is left in the original device.
+    Returns:
+        Tuple of Converted array.
+            (input_sent_batch_array, target_sent_batch_input_array,
+            target_sent_batch_output_array).
+            The shape of each array is `(batchsize, max_sentence_length)`.
+            All sentences are padded with -1 to reach max_sentence_length.
+    """
+
+    x_seqs, y_seqs = zip(*xy_batch)
+
+    x_block = convert.concat_examples(x_seqs, device, padding=-1)
+    y_block = convert.concat_examples(y_seqs, device, padding=-1)
+    xp = cuda.get_array_module(x_block)
+
+    x_block = xp.pad(x_block, ((0, 0), (0, 1)),
+                     'constant', constant_values=-1)
+    for i_batch, seq in enumerate(x_seqs):
+        x_block[i_batch, len(seq)] = eos_id
+    x_block = xp.pad(x_block, ((0, 0), (1, 0)),
+                     'constant', constant_values=bos_id)
+
+    y_out_block = xp.pad(y_block, ((0, 0), (0, 1)),
+                         'constant', constant_values=-1)
+    for i_batch, seq in enumerate(y_seqs):
+        y_out_block[i_batch, len(seq)] = eos_id
+
+    y_in_block = xp.pad(y_block, ((0, 0), (1, 0)),
+                        'constant', constant_values=bos_id)
+    return (x_block, y_in_block, y_out_block)
+
+
+class CalculateBleu(chainer.training.Extension):
+
+    trigger = 1, 'epoch'
+    priority = chainer.training.PRIORITY_WRITER
+
+    def __init__(
+            self, model, test_data, key, batch=100, device=-1, max_length=100):
+        self.model = model
+        self.test_data = test_data
+        self.key = key
+        self.batch = batch
+        self.device = device
+        self.max_length = max_length
+
+    def __call__(self, trainer):
+        with chainer.no_backprop_mode():
+            references = []
+            hypotheses = []
+            for i in range(0, len(self.test_data), self.batch):
+                sources, targets = zip(*self.test_data[i:i + self.batch])
+                references.extend([[t.tolist()] for t in targets])
+
+                sources = [
+                    chainer.dataset.to_device(self.device, x) for x in sources]
+                ys = [y.tolist()
+                      for y in self.model.translate(sources, self.max_length)]
+                hypotheses.extend(ys)
+
+        bleu = bleu_score.corpus_bleu(
+            references, hypotheses,
+            smoothing_function=bleu_score.SmoothingFunction().method1)
+        chainer.report({self.key: bleu})
+
+
+def count_lines(path):
+    with open(path) as f:
+        return sum([1 for _ in f])
+
+
+def load_vocabulary(path):
+    with open(path) as f:
+        # +3 for UNK, EOS, and BOS
+        word_ids = {line.strip(): i + 3 for i, line in enumerate(f)}
+    word_ids['<UNK>'] = 0
+    word_ids['<EOS>'] = 1
+    word_ids['<BOS>'] = 2
+    return word_ids
+
+
+def load_data(vocabulary, path):
+    n_lines = count_lines(path)
+    bar = progressbar.ProgressBar()
+    data = []
+    print('loading...: %s' % path)
+    with open(path) as f:
+        for line in bar(f, max_value=n_lines):
+            words = line.strip().split()
+            array = numpy.array([vocabulary.get(w, UNK) for w in words], 'i')
+            data.append(array)
+    return data
+
+
+def calculate_unknown_ratio(data):
+    unknown = sum((s == UNK).sum() for s in data)
+    total = sum(s.size for s in data)
+    return unknown / total
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Chainer example: seq2seq')
+    parser.add_argument('SOURCE', help='source sentence list')
+    parser.add_argument('TARGET', help='target sentence list')
+    parser.add_argument('SOURCE_VOCAB', help='source vocabulary file')
+    parser.add_argument('TARGET_VOCAB', help='target vocabulary file')
+    parser.add_argument('--validation-source',
+                        help='source sentence list for validation')
+    parser.add_argument('--validation-target',
+                        help='target sentence list for validation')
+    parser.add_argument('--batchsize', '-b', type=int, default=64,
+                        help='number of sentence pairs in each mini-batch')
+    parser.add_argument('--epoch', '-e', type=int, default=20,
+                        help='number of sweeps over the dataset to train')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--resume', '-r', default='',
+                        help='resume the training from snapshot')
+    parser.add_argument('--unit', '-u', type=int, default=1024,
+                        help='number of units')
+    parser.add_argument('--layer', '-l', type=int, default=3,
+                        help='number of layers')
+    parser.add_argument('--min-source-sentence', type=int, default=1,
+                        help='minimium length of source sentence')
+    parser.add_argument('--max-source-sentence', type=int, default=50,
+                        help='maximum length of source sentence')
+    parser.add_argument('--min-target-sentence', type=int, default=1,
+                        help='minimium length of target sentence')
+    parser.add_argument('--max-target-sentence', type=int, default=50,
+                        help='maximum length of target sentence')
+    parser.add_argument('--log-interval', type=int, default=200,
+                        help='number of iteration to show log')
+    parser.add_argument('--validation-interval', type=int, default=4000,
+                        help='number of iteration to evlauate the model '
+                        'with validation dataset')
+    parser.add_argument('--out', '-o', default='result',
+                        help='directory to output the result')
+    args = parser.parse_args()
+
+    source_ids = load_vocabulary(args.SOURCE_VOCAB)
+    target_ids = load_vocabulary(args.TARGET_VOCAB)
+    train_source = load_data(source_ids, args.SOURCE)
+    train_target = load_data(target_ids, args.TARGET)
+    assert len(train_source) == len(train_target)
+    train_data = [(s, t)
+                  for s, t in six.moves.zip(train_source, train_target)
+                  if args.min_source_sentence <= len(s)
+                  <= args.max_source_sentence and
+                  args.min_source_sentence <= len(t)
+                  <= args.max_source_sentence]
+    train_source_unknown = calculate_unknown_ratio(
+        [s for s, _ in train_data])
+    train_target_unknown = calculate_unknown_ratio(
+        [t for _, t in train_data])
+
+    print('Source vocabulary size: %d' % len(source_ids))
+    print('Target vocabulary size: %d' % len(target_ids))
+    print('Train data size: %d' % len(train_data))
+    print('Train source unknown ratio: %.2f%%' % (train_source_unknown * 100))
+    print('Train target unknown ratio: %.2f%%' % (train_target_unknown * 100))
+
+    target_words = {i: w for w, i in target_ids.items()}
+    source_words = {i: w for w, i in source_ids.items()}
+
+    model = AttentionSeq2seq(args.layer, len(source_ids), len(target_ids), args.unit)
+    if args.gpu >= 0:
+        chainer.cuda.get_device(args.gpu).use()
+        model.to_gpu(args.gpu)
+
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(model)
+
+    train_iter = chainer.iterators.SerialIterator(train_data, args.batchsize)
+    updater = training.StandardUpdater(
+        train_iter, optimizer,
+        converter=seq2seq_pad_concat_convert, device=args.gpu)
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'))
+    trainer.extend(extensions.LogReport(
+        trigger=(args.log_interval, 'iteration')))
+    trainer.extend(extensions.PrintReport(
+        ['epoch', 'iteration', 'main/loss', 'validation/main/loss',
+         'main/perp', 'validation/main/perp', 'validation/main/bleu',
+         'elapsed_time']),
+        trigger=(args.log_interval, 'iteration'))
+
+    if args.validation_source and args.validation_target:
+        test_source = load_data(source_ids, args.validation_source)
+        test_target = load_data(target_ids, args.validation_target)
+        assert len(test_source) == len(test_target)
+        test_data = list(six.moves.zip(test_source, test_target))
+        test_data = [(s, t) for s, t in test_data if 0 < len(s) and 0 < len(t)]
+        test_source_unknown = calculate_unknown_ratio(
+            [s for s, _ in test_data])
+        test_target_unknown = calculate_unknown_ratio(
+            [t for _, t in test_data])
+
+        print('Validation data: %d' % len(test_data))
+        print('Validation source unknown ratio: %.2f%%' %
+              (test_source_unknown * 100))
+        print('Validation target unknown ratio: %.2f%%' %
+              (test_target_unknown * 100))
+
+        @chainer.training.make_extension()
+        def translate(trainer):
+            source, target = test_data[numpy.random.choice(len(test_data))]
+            result = model.translate([model.xp.array(source)])[0]
+
+            source_sentence = ' '.join([source_words[x] for x in source])
+            target_sentence = ' '.join([target_words[y] for y in target])
+            result_sentence = ' '.join([target_words[y] for y in result])
+            print('# source : ' + source_sentence)
+            print('#  result : ' + result_sentence)
+            print('#  expect : ' + target_sentence)
+
+        trainer.extend(
+            translate, trigger=(args.validation_interval, 'iteration'))
+        trainer.extend(
+            CalculateBleu(
+                model, test_data, 'validation/main/bleu', device=args.gpu),
+            trigger=(args.validation_interval, 'iteration'))
+
+    print('start training')
+    trainer.run()
+
+
+if __name__ == '__main__':
+    main()
