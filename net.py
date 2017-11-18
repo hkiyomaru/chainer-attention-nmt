905f5a0ef95df22430a08703d6f18da563f24ef5
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2017-11-18 23:14:43 +0900

    add scripts to train attention-based nmt

--- net.py
+++ net.py
@@ -1,173 +1,284 @@
+"""Attention-based seq2seq model."""
+import numpy as np
+
 import chainer
+from chainer import Variable
+from chainer import Parameter
 import chainer.functions as F
 import chainer.links as L
 
+PAD = -1
+UNK = 0
+EOS = 1
 
-class AttentionSeq2seq(chainer.Chain):
 
-    def __init__(self, n_layers, n_source_vocab, n_target_vocab, n_units):
-        super(AttentionSeq2seq, self).__init__()
+class Seq2seq(chainer.Chain):
+
+    def __init__(self, n_source_vocab, n_target_vocab,
+                 n_encoder_layers, n_encoder_units, n_encoder_dropout,
+                 n_decoder_units, n_attention_units, n_maxout_units):
+        super(Seq2seq, self).__init__()
         with self.init_scope():
-            self.embed_x = L.EmbedID(n_source_vocab, n_units, ignore_label=-1)
-            self.embed_y = L.EmbedID(n_target_vocab, n_units, ignore_label=-1)
-            self.encoder = Encoder(n_units)
-            self.decoder = Decoder(n_units)
-            self.W = L.Linear(n_units, n_target_vocab)
-        
-        self.n_units = n_units
+            self.encoder = Encoder(
+                n_source_vocab,
+                n_encoder_layers,
+                n_encoder_units,
+                n_encoder_dropout
+            )
+            self.decoder = Decoder(
+                n_target_vocab,
+                n_decoder_units,
+                n_attention_units,
+                n_encoder_units * 2,  # because of bi-directional lstm
+                n_maxout_units,
+            )
 
-    def __call__(self, xs, ys_in, ys_out):
-        # Both xs and ys_in are lists of arrays.
-        exs = self.embed_x(xs)
-        eys = self.embed_y(ys_in)
+    def __call__(self, xs, ys):
+        """Calculate loss between outputs and ys.
 
-        batch = len(xs)
-        # None represents a zero vector in an encoder.
-        hxs = self.encoder(exs)
-        os = self.decoder(eys, hxs)
+        Args:
+            xs: Source sentences.
+            ys: Target sentences.
 
-        # It is faster to concatenate data before calculating loss
-        # because only one matrix multiplication is called.
-        concat_os = F.concat(os, axis=0)
-        concat_ys_out = F.concat(ys_out, axis=0)
-        loss = F.sum(F.softmax_cross_entropy(
-            self.W(concat_os), concat_ys_out, reduce='no')) / batch
+        Returns:
+            loss: Cross-entoropy loss between outputs and ys.
 
+        """
+        batch_size = len(xs)
+
+        hxs = self.encoder(xs)
+        os = self.decoder(ys, hxs)
+
+        concatenated_os = F.concat(os, axis=0)
+        concatenated_ys = F.flatten(ys)
+        loss = F.sum(
+            F.softmax_cross_entropy(
+                concatenated_os, concatenated_ys, reduce='no', ignore_label=PAD
+            )
+        )
+        loss = loss / batch_size
         chainer.report({'loss': loss.data}, self)
-        n_words = concat_ys_out.shape[0]
-        perp = self.xp.exp(loss.data * batch / n_words)
+        n_words = len(self.xp.where(concatenated_ys.data != EOS)[0])
+        perp = self.xp.exp(loss.data * batch_size / n_words)
         chainer.report({'perp': perp}, self)
+
         return loss
 
-    def translate(self, xs, max_length=100):
-        batch = len(xs)
-        with chainer.no_backprop_mode(), chainer.using_config('train', False):
-            xs = [x[::-1] for x in xs]
-            exs = sequence_embed(self.embed_x, xs)
-            h, c, _ = self.encoder(None, None, exs)
-            ys = self.xp.full(batch, EOS, 'i')
-            result = []
-            for i in range(max_length):
-                eys = self.embed_y(ys)
-                eys = F.split_axis(eys, batch, 0)
-                h, c, ys = self.decoder(h, c, eys)
-                cys = F.concat(ys, axis=0)
-                wy = self.W(cys)
-                ys = self.xp.argmax(wy.data, axis=1).astype('i')
-                result.append(ys)
+    def translate(self, xs):
+        """Generate sentences based on xs.
 
-        result = cuda.to_cpu(self.xp.stack(result).T)
-
-        # Remove EOS taggs
-        outs = []
-        for y in result:
-            inds = numpy.argwhere(y == EOS)
-            if len(inds) > 0:
-                y = y[:inds[0, 0]]
-            outs.append(y)
-        return outs
-
-
-class Attention(chainer.Chain):
-    
-    def __init__(self, n_units):
-        super(Attention, self).__init__()
-        with self.init_scope():
-            self.eh = L.Linear(n_units * 2, n_units)  # forward + backward
-            self.dh = L.Linear(n_units, n_units)
-            self.hw = L.Linear(n_units, 1)
-        
-        self.n_units = n_units
-    
-    def __call__(self, hxs, hy):
-        """Calculate attention for source sequences based on decoder's context.
-        
         Args:
-            hxs: Encoder's hidden states.
-            dh: Decoder's hidden state.
-        
+            xs: Source sentences.
+
         Returns:
-            attention: Attention for each word of source sequences.
-        
+            ys: Generated target sentences.
+
         """
-        attention = None
-        # apply `eh` to context vector
+        with chainer.no_backprop_mode(), chainer.using_config('train', False):
+            hxs = self.encoder(xs)
+            ys = self.decoder.translate(hxs)
 
-        # apply `dh` to decoder state
-        
-        # calculate the sum
-        
-        # apply `hw` to calculate attention  
-
-        return attention
+        return ys
 
 
 class Encoder(chainer.Chain):
-    
-    def __init__(self, n_units):
+
+    def __init__(self, n_vocab, n_layers, n_units, dropout):
         super(Encoder, self).__init__()
         with self.init_scope():
-            self.forward = L.LSTM(n_units, n_units)
-            self.backward = L.LSTM(n_units, n_units)
-        
-        self.n_units = n_units
+            self.embed_x = L.EmbedID(n_vocab, n_units, ignore_label=-1)
+            self.bilstm = L.NStepBiLSTM(n_layers, n_units, n_units, dropout)
 
-    def __call__(self, exs):
-        """Encode source sequences into the hidden vectors.
+    def __call__(self, xs):
+        """Encode source sequences into the representations.
 
         Args:
-            xs: Mini-batch of source sequences.
-        
+            xs: Source sequences.
+
         Returns:
             hxs: Hidden states for source sequences.
 
         """
-        self.__reset_state()
+        batch_size, max_length = xs.shape
 
-        exs = F.separate(exs, axis=1)
+        exs = self.embed_x(xs)
+        exs = F.separate(exs, axis=0)
+        masks = self.xp.vsplit(xs != -1, batch_size)
+        masked_exs = [ex[mask.reshape((-1, ))] for ex, mask in zip(exs, masks)]
+        masked_exs = sorted(masked_exs, key=lambda x: x.shape[0], reverse=True)
 
-        fhxs = self.__encode(exs, self.forward)
-        bhxs = reversed(self.__encode(reversed(exs), self.backward))
-        hxs = [F.concat([fh, bh]) for fh, bh in zip(fhxs, bhxs)]
+        _, _, hxs = self.bilstm(None, None, masked_exs)
+        hxs = F.pad_sequence(hxs, length=max_length, padding=0.0)
 
         return hxs
 
-    def __encode(self, exs, transformer):
-        states = []
-        for ex in exs:
-            states.append(transformer(ex))
-        return states
-
-    def __reset_state(self):
-        self.forward.reset_state()
-        self.backward.reset_state()
-
 
 class Decoder(chainer.Chain):
-    
-    def __init__(self, n_units):
+
+    def __init__(self, n_vocab, n_units, n_attention_units,
+                 n_encoder_output_units, n_maxout_units, n_maxout_pools=2):
         super(Decoder, self).__init__()
         with self.init_scope():
-            self.forward = L.LSTM(n_units, n_units)
-            self.attention = Attention(n_units)
-
+            self.embed_y = L.EmbedID(n_vocab, n_units, ignore_label=-1)
+            self.lstm = L.StatelessLSTM(
+                n_units + n_encoder_output_units,
+                n_units
+            )
+            self.maxout = L.Maxout(
+                n_units + n_encoder_output_units + n_units,
+                n_maxout_units,
+                n_maxout_pools
+            )
+            self.w = L.Linear(n_units, n_vocab)
+            self.attention = AttentionModule(
+                n_encoder_output_units,
+                n_attention_units,
+                n_units
+            )
+            self.bos_state = Parameter(
+                initializer=self.xp.random.randn(1, n_units).astype('f')
+            )
         self.n_units = n_units
 
-    def __call__(self, eys, hxs):
-        """Encode source sequences into the hidden vectors.
+    def __call__(self, ys, hxs):
+        """Calculate cross-entoropy loss between predictions and ys.
 
         Args:
-            eys: Mini-batch of target sequences.
-            hxs: Annotation-vector of source sentences.
+            ys: Target sequences.
+            hxs: Hidden states for source sequences.
 
         Returns:
-          dhs: Hidden states for target sequences.
+            loss: Cross-entoropy loss between predictions and ys.
 
         """
-        eys = F.separate(eys, axis=1)
-        for ey in eys:
-            attention = self.attention()
+        batch_size, max_length, encoder_output_size = hxs.shape
 
-    def __decode(self, eys, hxs):
+        compute_context = self.attention(hxs)
+        # initial cell state
+        c = Variable(self.xp.zeros((batch_size, self.n_units), 'f'))
+        # initial hidden state
+        h = F.broadcast_to(self.bos_state, ((batch_size, self.n_units)))
+        # initial character's embedding
+        previous_embedding = self.embed_y(
+            Variable(self.xp.full((batch_size, ), EOS, 'i'))
+        )
+
         os = []
-        for 
+        for y in self.xp.hsplit(ys, ys.shape[1]):
+            y = y.reshape((batch_size, ))
+            context = compute_context(h)
+            concatenated = F.concat((previous_embedding, context))
+
+            c, h = self.lstm(c, h, concatenated)
+            concatenated = F.concat((concatenated, h))
+
+            os.append(self.w(self.maxout(concatenated)))
+
+        return os
+
+    def translate(self, hxs, max_length=100):
+        """Generate target sentences given hidden states of source sentences.
+
+        Args:
+            hxs: Hidden states for source sequences.
+
+        Returns:
+            ys: Generated sentence
+
+        """
+        batch_size, _ = hxs.shape
+        compute_context = self.attention(hxs)
+        c = Variable(self.xp.zeros((batch_size, self.n_units), 'f'))
+        h = F.broadcast_to(self.bos_state, ((batch_size, self.n_units)))
+        # first character's embedding
+        previous_embedding = self.embed_y(
+            Variable(self.xp.full((batch_size, ), EOS, 'i'))
+        )
+
+        results = []
+        for _ in range(max_length):
+            context = compute_context(h)
+            concatenated = F.concat((previous_embedding, context))
+
+            c, h = self.lstm(c, h, concatenated)
+            concatenated = F.concat((concatenated, h))
+
+            logit = self.w(self.maxout(concatenated))
+
+            results.append(F.reshape(F.argmax(logit, axis=1), (batch_size, )))
+        else:
+            results = F.separate(F.transpose(F.vstack(results)), axis=0)
+
+        ys = []
+        for result in results:
+            index = np.argwhere(result.data == EOS)
+            if len(index) > 0:
+                result = result[:index[0, 0] + 1]
+            ys.append(result.data)
+
+        return ys
+
+
+class AttentionModule(chainer.Chain):
+
+    def __init__(self, n_encoder_output_units,
+                 n_attention_units, n_decoder_units):
+        super(AttentionModule, self).__init__()
+        with self.init_scope():
+            self.h = L.Linear(n_encoder_output_units, n_attention_units)
+            self.s = L.Linear(n_decoder_units, n_attention_units)
+            self.o = L.Linear(n_attention_units, 1)
+        self.n_encoder_output_units = n_encoder_output_units
+        self.n_attention_units = n_attention_units
+
+    def __call__(self, hxs):
+        """Returns a function that calculates context given decoder's state.
+
+        Args:
+            hxs: Encoder's hidden states.
+
+        Returns:
+            compute_context: A function to calculate attention.
+
+        """
+        batch_size, max_length, encoder_output_size = hxs.shape
+
+        encoder_factor = F.reshape(
+            self.h(
+                F.reshape(
+                    hxs,
+                    (batch_size * max_length, self.n_encoder_output_units)
+                )
+            ),
+            (batch_size, max_length, self.n_attention_units)
+        )
+
+        def compute_context(previous_state):
+            decoder_factor = F.broadcast_to(
+                F.reshape(
+                    self.s(previous_state),
+                    (batch_size, 1, self.n_attention_units)
+                ),
+                (batch_size, max_length, self.n_attention_units)
+            )
+
+            attention = F.softmax(
+                F.reshape(
+                    self.o(
+                        F.reshape(
+                            F.tanh(encoder_factor + decoder_factor),
+                            (batch_size * max_length, self.n_attention_units)
+                        )
+                    ),
+                    (batch_size, max_length)
+                )
+            )
+
+            context = F.reshape(
+                F.batch_matmul(attention, hxs, transa=True),
+                (batch_size, encoder_output_size)
+            )
+
+            return context
+
+        return compute_context
